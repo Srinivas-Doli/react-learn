7f1095b50e51d591dc135cbb4f07300bca2f825b
Srinivas-Doli <dolisrinivas4280@gmail.com>
2026-07-05 15:42:50 +0530

    Removed Unnessasary things

--- App.js
+++ App.js
@@ -1,16 +1,3 @@
-/**
- * <div id="parent">
- * <div id="child">
- * <h1>I'm the h1 Tag</h1>
- * <h2>I'm the h2 Tag</h2>
- * </div>
- * <div id="child2">
- * <h1>I'm the h1 Tag</h1>
- * <h2>I'm the h2 Tag</h2>
- * </div>
- * </div>
- */
-
 const parent =React.createElement("div", {id:"parent"}, 
               [React.createElement("div", {id:"child"},
             [React.createElement("h1", {}, "I'm the h1 Tag"),React.createElement("h2", {},"I'm the h2 Tag")]),
